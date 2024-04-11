5cc1a68b0c203770ba6913c93369cacf9ef52cca
马超 <42859321+Mica-Ma@users.noreply.github.com>
2024-04-11 15:29:20 +0800

    Update sojan.js

--- sojan.js
+++ sojan.js
@@ -4,22 +4,22 @@
  * @LastEditors: machao
  * @LastEditTime: 2024-04-11 15:09:30
  */
-const CHECK_URL = 'api.sojan.top:3363/app-config/info'
+const CHECK_URL = "api.sojan.top:3363/app-config/info";
 
-const url = $request.url
-const isCheckUrl = (url) => url.includes(CHECK_URL)
+const url = $request.url;
+const isCheckUrl = (url) => url.includes(CHECK_URL);
 
 if (isCheckUrl(url) && $response.status != 200) {
   const unlock = {
-    notice: '赶紧去注册',
+    notice: "赶紧去注册",
     canRegister: true,
-  }
+  };
 
-  const status = 200
-  const headers = $response.headers
-  const body = JSON.stringify(unlock)
+  const status = 200;
+  const headers = $response.headers;
+  const body = JSON.stringify(unlock);
 
-  $done({ status, headers, body })
+  $done({ status, headers, body });
 } else {
-  $done({})
+  $done({});
 }
