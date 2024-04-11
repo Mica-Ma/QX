e6c7a35f0b99807e185489d225283392fb0aa9f1
马超 <42859321+Mica-Ma@users.noreply.github.com>
2024-04-11 15:13:02 +0800

    Add files via upload

---
+++ sojan.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: machao
+ * @Date: 2024-04-11 14:37:55
+ * @LastEditors: machao
+ * @LastEditTime: 2024-04-11 15:09:30
+ */
+const CHECK_URL = 'api.sojan.top:3363/app-config/info'
+
+const url = $request.url
+const isCheckUrl = (url) => url.includes(CHECK_URL)
+
+if (isCheckUrl(url) && $response.status != 200) {
+  const unlock = {
+    notice: '赶紧去注册',
+    canRegister: true,
+  }
+
+  const status = 200
+  const headers = $response.headers
+  const body = JSON.stringify(unlock)
+
+  $done({ status, headers, body })
+} else {
+  $done({})
+}
